ec51a8c962d44d6f65780ee2afb6cff6b7a0d37c
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-11-25 12:04:18 +0100

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -21,15 +21,38 @@ class InputSchema(BaseModel):
         }
 
 
+fake_db = {
+    1: {
+        "track": "Data Science",
+        "mentor": "Chineidu",
+        "cost": 350_000.0,
+        "company": "Stutern",
+    },
+    2: {
+        "track": "Software Engineering",
+        "mentor": "Olamide",
+        "cost": 380_000.0,
+        "company": "Stutern",
+    },
+}
+
+
 @app.get("/")
 def root():
     """This is the root."""
     return {"message": "This is the homepage."}
 
+@app.get("/fetch-all/")
+def fetch_all():
+    """This is used to fetch all the tracks."""
+    return fake_db
 
-@app.post("/signup")
-def signup(input_: InputSchema):
-    return input_  # OR return {"message": input_.track, "mentor": input_.mentor, "cost": input_.cost}
+
+@app.get("/fetch-one/{id}")
+def fetch_one(id: int):
+    for idx, val in fake_db.items():
+        if idx == id:
+            return val
 
 
 if __name__ == "__main__":
