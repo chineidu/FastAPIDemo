41c0f79a60e286a18bdbdb4deaaf686eebb70dbd
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-12-01 14:38:29 +0100

    Convert the fake_db ids to string

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
+import typing as tp 
 app = FastAPI()
 
 
@@ -9,7 +11,6 @@ class InputSchema(BaseModel):
     mentor: str
     cost: float
     company: str
-
     class Config:
         schema_extra = {
             "example": {
@@ -19,16 +20,17 @@ class InputSchema(BaseModel):
                 "company": "Stutern",
             }
         }
-
+class DBSchema(BaseModel):
+    key: tp.Dict[str, InputSchema]
 
 fake_db = {
-    1: {
+    "1": {
         "track": "Data Science",
         "mentor": "Chineidu",
         "cost": 350_000.0,
         "company": "Stutern",
     },
-    2: {
+    "2": {
         "track": "Software Engineering",
         "mentor": "Olamide",
         "cost": 380_000.0,
@@ -38,21 +40,48 @@ fake_db = {
 
 
 @app.get("/")
-def root():
-    """This is the root."""
-    return {"message": "This is the homepage."}
+def index(request: Request) -> tp.Any:
+    """Basic HTML response."""
 
-@app.get("/fetch-all/")
-def fetch_all():
+    body = ("""
+            <html>
+                <body style='padding: 15px;'>
+                    <h1>Welcome to the API</h1>
+                    <div>
+                    Check the docs: <a href='/docs'>here</a>
+                    </div>
+                </body>
+            </html>
+
+
+            </html>
+    """)
+
+    return HTMLResponse(content=body)
+
+
+@app.get("/fetch-all/", response_model=None)
+def fetch_all() -> tp.Dict:
     """This is used to fetch all the tracks."""
     return fake_db
 
 
-@app.get("/fetch-one/{id}")
-def fetch_one(id: int):
+@app.get("/fetch-one/{id}/", response_model=InputSchema)
+def fetch_one(id: int)-> tp.Dict:
     for idx, val in fake_db.items():
-        if idx == id:
+        if int(idx) == id:
             return val
+
+@app.post("/add-track/{id}", response_model=InputSchema)
+def add_track(id: int, data:InputSchema) -> tp.Dict:
+    """This is used to add a new track and other details
+    to the fake_db."""
+    ids = fake_db.keys()
+    ids = [int(idx) for idx in ids] # Convert to ids to int
+    if id not in ids:
+        # Add the new data
+        fake_db[str(id)] = data
+    return data
 
 
 if __name__ == "__main__":
